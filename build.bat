fff4a264c87f311fae948ff02ee31c4b9e74fbd4
Jaroslav Záruba <jaroslav.zaruba@gmail.com>
2017-05-17 08:36:17 +0200

    .

---
+++ build.bat
@@ -0,0 +1,18 @@
+@ECHO Off
+
+call env
+
+if "%JAVA8_HOME%" == "" (
+  echo Unable to determine JAVA 8 Home ^(Environmental variable %%^JAVA8_HOME%% not defined^)
+  echo.
+  pause
+  exit /b 1
+)
+
+set JAVA_HOME=%JAVA8_HOME%
+
+call gradlew.bat createJar
+
+if %ERRORLEVEL% == 0 (
+  copy usbserial\eclipse_lib\usbserial.jar ..\CAN_Bus\app\libs\
+)
